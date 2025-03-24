a644eedfe5dbd5d85f2072538beb8111610ec3f9
OneTen19 <khy000426@naver.com>
2025-03-24 20:27:16 +0900

    #3.5 Reusable Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -67,26 +67,45 @@ class MyApp extends StatelessWidget {
                 height: 30,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(45),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 50, vertical: 20),
-                      child: Text(
-                        "Transfer",
-                        style: TextStyle(
-                          fontSize: 22,
-                        ),
-                      ),
-                    ),
-                  )
+                  MyButton(text: "Transfer", bgColor: Colors.amber, textColor: Colors.black),
+                  MyButton(text: "Request", bgColor: Colors.grey.shade800, textColor: Colors.white,),
+
                 ],
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  final String text;
+  final Color bgColor;
+  final Color textColor;
+
+  const MyButton({
+    super.key, required this.text, required this.bgColor, required this.textColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: 45, vertical: 15),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: textColor,
+            fontSize: 22,
           ),
         ),
       ),
